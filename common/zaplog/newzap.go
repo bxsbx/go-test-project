6a42769777a41c4f57c4b29f9ecfa1894a688307
linhaiming <1329857853@qq.com>
2024-02-23 19:35:39 +0800

    fix:初始化日志修改

--- common/zaplog/newzap.go
+++ common/zaplog/newzap.go
@@ -1,13 +1,29 @@
 package zaplog
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"io"
+)
 
-func InitZap() (err error) {
-	Logger, err = writeToConsole()
-	return
-}
+func InitZap(level zapcore.Level, write io.Writer) *zap.Logger {
+	//配置日志输入位置
+	writeSyncer := zapcore.AddSync(write)
+	//配置日志打印格式
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoder := zapcore.NewConsoleEncoder(encoderConfig) //美化格式
+	//encoder := zapcore.NewJSONEncoder(encoderConfig) //json格式
+	//配置日志级别
+	core := zapcore.NewCore(encoder, writeSyncer, level)
 
-// 直接输出到控制台
-func writeToConsole() (*zap.Logger, error) {
-	return zap.NewProductionConfig().Build(zap.AddCallerSkip(1))
+	//打印调用函数
+	caller := zap.AddCaller()
+	//打印调用函数时，跳过几个函数
+	skip := zap.AddCallerSkip(1)
+	//打印调用栈信息（error级别才打印）
+	stacktrace := zap.AddStacktrace(zap.ErrorLevel)
+	//获取日志对象
+	return zap.New(core, caller, skip, stacktrace)
 }
--- test/GPT.go
+++ test/GPT.go
@@ -1,1 +1,65 @@
 package main
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+func Pring(logger *zap.Logger) {
+	field := zap.Any("header", "req.Header")
+	// 记录不同级别的日志
+	logger.Debug("This is a debug log", field)
+	logger.Info("This is an info log", field)
+	logger.Warn("This is a warning log", field)
+	logger.Error("This is an error log", field)
+}
+
+func Pring2(logger *zap.SugaredLogger) {
+	field := zap.Any("header", "req.Header")
+	// 记录不同级别的日志
+	logger.Debug("This is a debug log", field)
+	logger.Info("This is an info log", field)
+	logger.Warn("This is a warning log", field)
+	logger.Error("This is an error log", field)
+}
+
+func main() {
+
+	// 配置日志级别
+	//atomicLevel := zap.NewAtomicLevel()
+	//
+	//// 设置日志级别为Debug
+	//atomicLevel.SetLevel(zapcore.DebugLevel)
+	//
+	//// 创建配置
+	//config := zap.NewProductionConfig()
+	//config.Level = atomicLevel
+	//
+	//// 构建Logger
+	//logger, _ := config.Build()
+	//Pring(logger)
+
+	//file, _ := os.OpenFile("filePath.text", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	//logger := log.New(os.Stdout, "prelog", log.LstdFlags)
+	//logger.Println("cwqwfqwf")
+	//
+	//log.Println()
+
+	writeSyncer := zapcore.AddSync(os.Stdout)
+
+	//encoder := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoder := zapcore.NewConsoleEncoder(encoderConfig)
+
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.InfoLevel))
+	sugaredLogger := logger.Sugar()
+	Pring(logger)
+	Pring2(sugaredLogger)
+
+}
